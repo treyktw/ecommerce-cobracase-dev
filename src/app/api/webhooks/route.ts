cb2142fbb6e825d40198ef40652dc3add401b9a1
treyktw <101672317+treyktw@users.noreply.github.com>
2024-05-17 01:02:03 -0400

    email commit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,6 +3,10 @@ import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { Resend } from "resend"
+import OrderReceivedEmail from "@/components/emails/order-received-email";
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: Request) {
   try {
@@ -66,7 +70,28 @@ export async function POST(req: Request) {
           },
         },
       })
+
+      await resend.emails.send({
+        from: 'CaseCobra <teleraventures@gmail.com>',
+        to: [event.data.object.customer_details.email],
+        subject: 'Thanks for your order!',
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state,
+          },
+        }),
+      })
     }
+
+    
     return NextResponse.json({ result: event, ok: true })
   } catch (err) {
     console.error(err)
